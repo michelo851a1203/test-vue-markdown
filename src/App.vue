3017c381363d9f71588619cf8313ff50ea460d6a
MichaelHo <michael@paradromix.com>
2022-11-30 17:49:13 +0800

    init # 20221130001

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import MarkdownIt from 'markdown-it';
+import { ref, computed } from 'vue';
+const inputMarkdown = ref('')
+
+const markdownToHtml = computed(() => {
+  const markdownResult = new MarkdownIt();
+  const convertedStringFromMarkdown = markdownResult.render(inputMarkdown.value);
+  return convertedStringFromMarkdown;
+})
+
+</script>
+
+<template>
+  <div>
+    <textarea
+      class="
+        w-2/3 h-48 border border-gray-800 
+        focus:outline-none
+      "
+      v-model="inputMarkdown"
+    ></textarea>
+  </div>
+  <div
+    class="w-full bg-gray-200 px-3 py-2 box-border markdown-limit"
+    v-html="markdownToHtml">
+  </div>
+</template>
+
+<style scoped></style>
