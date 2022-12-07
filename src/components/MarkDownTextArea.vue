fb9022f53c77a40f082f8b236a55835b3a8e752d
MichaelHo <michael@paradromix.com>
2022-12-07 19:14:15 +0800

    - feat: append plugin and solution # 20221207001 	修改：     README.md 	修改：     src/components/MarkDownTextArea.vue 	修改：     src/composable/markdownTextArea.ts

--- src/components/MarkDownTextArea.vue
+++ src/components/MarkDownTextArea.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import { useMarkDownTextArea } from '../composable/markdownTextArea';
 const {
+  inputMarkdown,
   markdownTextArea,
   markdownToHtml,
+  addBoldBlock,
+  addItalicBlock,
+  addUnderlineBlock,
+  addSlashLineBlock,
+  addBlockQuoteBlock,
+  unknownUsageBlock,
+  addNumberListBlock,
+  addDotListBlock,
+  addHeaderBlock,
+  addLinkBlock,
+  addImgBlock,
 } = useMarkDownTextArea()
-
-
 </script>
-
 
 <template>
   <section
@@ -17,45 +26,56 @@ const {
       class="w-full box-border px-3 py-2 border-b-2 border-gray-300 space-x-3 flex items-center"
     >
       <button
+        @click="addBoldBlock"
         class="i-fa6-solid-b font-bold w-5 h-5 cursor-pointer"
       ></button>
       <button
+        @click="addItalicBlock"
         class="i-ooui-italic-i font-bold w-5 h-5 cursor-pointer"
       >
       </button>
       <button
+        @click="addUnderlineBlock"
         class="i-mdi-format-underline font-bold w-6 h-6 cursor-pointer"
       >
       </button>
       <button
+        @click="addSlashLineBlock"
         class="i-fluent-text-strikethrough-16-filled font-bold w-6 h-6 cursor-pointer"
       >
       </button>
       <button
+        @click="addBlockQuoteBlock"
         class="i-material-symbols-format-quote-rounded font-bold w-6 h-6 cursor-pointer"
       >
       </button>
       <button
+        @click="unknownUsageBlock"
         class="i-majesticons-underline-2 font-bold w-6 h-6 cursor-pointer"
       >
       </button>
       <button
+        @click="addNumberListBlock"
         class="i-material-symbols-format-list-numbered-rounded font-bold w-6 h-6 cursor-pointer"
       >
       </button>
       <button
+        @click="addDotListBlock"
         class="i-material-symbols-format-list-bulleted font-bold w-6 h-6 cursor-pointer"
       >
       </button>
       <button
+        @click="addHeaderBlock"
         class="i-ph-text-h-bold font-bold w-6 h-6 cursor-pointer"
       >
       </button>
       <button
+        @click="addLinkBlock"
         class="i-mingcute-link-fill font-bold w-6 h-6 cursor-pointer"
       >
       </button>
       <button
+        @click="addImgBlock"
         class="i-material-symbols-broken-image-outline font-bold w-6 h-6 cursor-pointer"
       >
       </button>
@@ -67,10 +87,11 @@ const {
         ref="markdownTextArea"
         placeholder="請輸入內容"
         class="
-          w-[calc(95%_+_2.6rem)]
+          w-full
           h-48 px-3 py-2
           focus:outline-none
           border-none
+          box-border
           text-lg
         "
         v-model="inputMarkdown"
@@ -83,6 +104,4 @@ const {
     class="w-full bg-gray-200 px-3 py-2 box-border markdown-limit"
     v-html="markdownToHtml">
   </div>
-
-
 </template>
--- src/composable/markdownTextArea.ts
+++ src/composable/markdownTextArea.ts
@@ -1,15 +1,49 @@
-import { ref, Ref, computed } from 'vue';
+import { ref, Ref, computed, ComputedRef } from 'vue';
 import MarkdownIt from 'markdown-it';
+import Token from 'markdown-it/lib/token';
+import Renderer from 'markdown-it/lib/renderer';
 
+export interface UseMarkdownTextAreaOptions {
+  BoldTextInputTip?: string;
+}
 
-export function useMarkDownTextArea() {
+export function useMarkDownTextArea(
+  textAreaOptions?: UseMarkdownTextAreaOptions,
+) {
   const markdownTextArea: Ref<HTMLTextAreaElement | null> = ref(null);
   const inputMarkdown = ref('');
 
+  const underlinePlugins = (
+    tokens: Token[],
+    idx: number,
+    options: MarkdownIt.Options,
+    _: any,
+    self: Renderer 
+  ): string =>  {
+    const token = tokens[idx];
+    if (token.markup === '+') {
+      token.tag = 'u';
+    }
+    return self.renderToken(tokens, idx, options);
+  }
+
+  const markDownUnderline = (md: MarkdownIt): void => {
+    md.renderer.rules.em_open = underlinePlugins;
+    md.renderer.rules.em_close = underlinePlugins;
+  }
+
   const markdownToHtml = computed(() => {
     const markdownResult = new MarkdownIt();
+    markdownResult.use(markDownUnderline);
     const convertedStringFromMarkdown = markdownResult.render(inputMarkdown.value);
     return convertedStringFromMarkdown;
+  });
+
+  const textAreaRef: ComputedRef<HTMLTextAreaElement>  = computed(() => {
+    const markdownTextAreaOrNull = markdownTextArea.value;
+    if (markdownTextAreaOrNull === null) throw 'not get textArea';
+    const textArea = markdownTextAreaOrNull as HTMLTextAreaElement;
+    return textArea;
   });
 
   const insertAtCursor = (insertText: string) => {
@@ -25,9 +59,139 @@ export function useMarkDownTextArea() {
     inputMarkdown.value += insertText;
   }
 
+  const checkIsMultipleLine = () => {
+    const regularExpression = new RegExp('\n','gm')
+    return regularExpression.test(inputMarkdown.value);
+  }
+
+  const isSelectedTextAreaText = () => {
+    const textArea = textAreaRef.value
+    return textArea.selectionStart !== textArea.selectionEnd;
+  }
+
+  const appendBoldBlockLastLine = () => {
+    const textArea = textAreaRef.value;
+    const tip = textAreaOptions?.BoldTextInputTip ?? '(這裡輸入文字)';
+    if (inputMarkdown.value.length === 0) {
+      inputMarkdown.value += `**${tip}**`;
+    } else {
+      inputMarkdown.value += `  \n**${tip}**`;
+    }
+
+    setTimeout(() => {
+      const cursorEndPosition = inputMarkdown.value.length - 2;
+      const cursorStartPosition = inputMarkdown.value.length - 2 - tip.length;
+      textArea.setSelectionRange(cursorStartPosition, cursorEndPosition);
+      textArea.focus();
+    }, 0);
+  }
+
+  const addBoldBlock = () => {
+    const textArea = textAreaRef.value;
+    if (isSelectedTextAreaText()) return;
+    appendBoldBlockLastLine()
+  }
+
+  const appendItalicBlockLastLine = () => {
+    const textArea = textAreaRef.value;
+    const tip = textAreaOptions?.BoldTextInputTip ?? '(這裡輸入文字)';
+    if (inputMarkdown.value.length === 0) {
+      inputMarkdown.value += `*${tip}*`;
+    } else {
+      inputMarkdown.value += `  \n*${tip}*`;
+    }
+
+    setTimeout(() => {
+      const cursorEndPosition = inputMarkdown.value.length - 2;
+      const cursorStartPosition = inputMarkdown.value.length - 2 - tip.length;
+      textArea.setSelectionRange(cursorStartPosition, cursorEndPosition);
+      textArea.focus();
+    }, 0);
+  }
+
+  const addItalicBlock = () => {
+    const textArea = textAreaRef.value;
+    if (isSelectedTextAreaText()) return;
+    appendItalicBlockLastLine()
+  }
+
+  const addUnderlineBlock = () => {
+    console.log('add underline block');
+  }
+
+
+  const appendSlashBlockLastLine = () => {
+    const textArea = textAreaRef.value;
+    const tip = textAreaOptions?.BoldTextInputTip ?? '(這裡輸入文字)';
+    if (inputMarkdown.value.length === 0) {
+      inputMarkdown.value += `~~${tip}~~`;
+    } else {
+      inputMarkdown.value += `  \n~~${tip}~~`;
+    }
+
+    setTimeout(() => {
+      const cursorEndPosition = inputMarkdown.value.length - 2;
+      const cursorStartPosition = inputMarkdown.value.length - 2 - tip.length;
+      textArea.setSelectionRange(cursorStartPosition, cursorEndPosition);
+      textArea.focus();
+    }, 0);
+  }
+
+  const addSlashLineBlock = () => {
+    const textArea = textAreaRef.value;
+    if (isSelectedTextAreaText()) return;
+    appendSlashBlockLastLine()
+
+  }
+
+  const addBlockQuoteBlock = () => {
+
+  }
+
+  const unknownUsageBlock = () => {
+    const markdownResult = new MarkdownIt();
+    markdownResult.use(markDownUnderline);
+    markdownResult.renderInline('++')
+
+
+  }
+
+  const addNumberListBlock = () => {
+
+  }
+
+  const addDotListBlock = () => {
+
+  }
+
+  const addHeaderBlock = () => {
+
+  }
+
+  const addLinkBlock = () => {
+
+  }
+
+  const addImgBlock = () => {
+
+  }
+  
+
   return {
+    inputMarkdown,
     markdownTextArea,
     markdownToHtml,
     insertAtCursor,
+    addBoldBlock,
+    addItalicBlock,
+    addUnderlineBlock,
+    addSlashLineBlock,
+    addBlockQuoteBlock,
+    unknownUsageBlock,
+    addNumberListBlock,
+    addDotListBlock,
+    addHeaderBlock,
+    addLinkBlock,
+    addImgBlock,
   }
 }
